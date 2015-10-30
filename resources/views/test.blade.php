030a0150f3009fb214d8a5b348128c6da74feb91
Chloe Lee <chloealessandra@gmail.com>
2015-10-30 08:22:27 -0400

    changes icon to hamburger on collapse

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -43,10 +43,7 @@
           <!-- Brand and toggle get grouped for better mobile display -->
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
+              <span class="sr-only">Toggle navigation</span><img src={{asset('images/hamburger2.png')}}>
             </button>
             <a class="navbar-brand" href="#"><img src={{asset('images/shine2_logo.png')}}></a>
           </div>
@@ -56,6 +53,7 @@
             <ul class="nav navbar-nav">
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src={{asset('images/hamburger2.png')}}></a>
+
                 <ul class="dropdown-menu">
                   <li><a href="#about">ABOUT</a></li>
                   <li><a href="#testimonials">TESTIMONIALS</a></li>
