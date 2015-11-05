75b787ceff66c0f4cdb41189185833480e7f9169
Chloe Lee <chloealessandra@gmail.com>
2015-11-04 21:41:23 -0500

    adds all sections with bootstrap

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -89,21 +89,21 @@
       </nav>
 
       <div class="header">    
-          <div class="text_bubble_column">
-              <div id="shine_text_bubble">
-                  <p>Welcome to Shine! We all need a little help to get through the day & feel our best self. We'll text you daily work hacks & reminders of why you're a badass.</p>
-              </div>
+        <div class="text_bubble_column">
+          <div id="shine_text_bubble">
+              <p>Welcome to Shine! We all need a little help to get through the day & feel our best self. We'll text you daily work hacks & reminders of why you're a badass.</p>
+          </div>
 
-              <div id="user_text_bubble">
-                  <p>So pumped!</p>
-              </div>
+          <div id="user_text_bubble">
+              <p>So pumped!</p>
+          </div>
 
-              <div id="shine_text_bubble_2">
-                  <p>Glad to hear it. You're going to crush it today. #ShineOn.</p>
-              </div>
-      </div>
+          <div id="shine_text_bubble_2">
+              <p>Glad to hear it. You're going to crush it today. #ShineOn.</p>
+          </div>
+        </div>
 
-      <div id="logo_column">
+        <div id="logo_column">
           <img src={{asset('images/shine_logo.png')}}>
 
           <h3>A daily text so you can slay today.</h3>
@@ -124,8 +124,9 @@
     
               <input type="submit" value="SIGNUP" class="signup_button"/>
           </form>
-          
-          <!-- <span style="font-size:80%"><a href="https://www.mobilecommons.com/about/terms-and-conditions/">Msg &amp; Data Rates May Apply.</a><br />Text <b>STOP</b> to opt out. No purchase necessary.<br />Expect 4 msgs/mo. <a href="https://www.mobilecommons.com/about/terms-and-conditions/privacy-policy/">Terms and Conditions</a></span> -->
+            
+            <!-- <span style="font-size:80%"><a href="https://www.mobilecommons.com/about/terms-and-conditions/">Msg &amp; Data Rates May Apply.</a><br />Text <b>STOP</b> to opt out. No purchase necessary.<br />Expect 4 msgs/mo. <a href="https://www.mobilecommons.com/about/terms-and-conditions/privacy-policy/">Terms and Conditions</a></span> -->
+        </div>
       </div>
 
       <a name="about"></a>
@@ -146,6 +147,83 @@
         <h3>SHARE WITH FRIENDS</h3>
         <p>The best part? You can share all of this motiv-affirmation (motivation + affirmation. Yeah, we made up a word.) with your friends to give them the boost they need. A few Channing Tatum GIFs never hurt anyone, right?</p>
       </div>
+
+      <div style="clear: both;"></div>
+      <a name="testimonials"></a>
+      <div class="testimonials">
+        <h2>WORD ON THE STREET</h2>
+        <div class="col-md-4" id="testimonials">
+          <img src={{asset('images/lindsay.png')}}>
+          <p>"This week was awesome, I loved hearing my text sound go off in the AM as I was getting ready for work. It's the perfect mix of being smart, funny, and intelligent and of course accessible! Shine is DOPE!"</p>
+          <p id="name">Lindsay, Ad Sales Planner</p>  
+        </div>
+
+        <div class="col-md-4" id="testimonials">
+          <img src={{asset('images/brenna.png')}}>
+          <p>"EVERYTHING I ever needed to make it through another awkward week of my version of adulting. I'm always twiddling my thumbs waiting for Shine to kick my ass back into gear. #dependency"</p> 
+          <p id="name">Brenna, Graduate Student</p>
+        </div>
+
+        <div class="col-md-4" id="testimonials">
+          <img src={{asset('images/jenni.png')}}>
+          <p>"Thank you so much for the positive energy. We live in such a stressful world. It's incredible what a few positive words or a funny picture can do to our psyche."</p>
+          <p id="name">Jenni, Dentist</p>
+        </div>
+
+      </div>
+
+      <div style="clear: both;">
+      <a name="refer"></a>
+      <div class="refer-a-friend">
+        <h2>Share Shine texts with your friends!</h2>
+        <form action="https://secure.mcommons.com/profiles/join" method="post">                    
+            <input type="hidden" name="opt_in_path[]" value="OP4B1A27AC508266A1F4373419CE1BE391" />
+                <input type="hidden" name="redirect_to" value={{ URL::route('refer_confirmation') }} />
+            
+            <div class="col-md-4" id="referral">
+                <input required="required" placeholder="YOUR NAME" type="text" name="person[first_name]" id="person_first_name" /></br></br>
+                <input required="required" placeholder="YOUR PHONE NUMBER" type="text" name="person[phone]" id="person_phone" /></br>
+            </div>
+
+            <div class="col-md-4" id="referral">
+                <input required="required" placeholder="YOUR FRIEND'S NUMBER" type="text" name="friends[]"/><br /> <input type="hidden" name="friends_opt_in_path" value="OPE8B3F738CF07CE0C3AFA3F45A5E155ED" /><br>
+                <input placeholder="YOUR FRIEND'S NUMBER (OPTIONAL)" type="text" name="friends[]"/><br /> <input type="hidden" name="friends_opt_in_path" value="OPE8B3F738CF07CE0C3AFA3F45A5E155ED" /><br>               
+            </div>
+
+            <div class="col-md-4" id="referral">            
+                <input placeholder="YOUR FRIEND'S NUMBER (OPTIONAL)" type="text" name="friends[]"/><br /> <input type="hidden" name="friends_opt_in_path" value="OPE8B3F738CF07CE0C3AFA3F45A5E155ED" /><br>
+                <input placeholder="YOUR FRIEND'S NUMBER (OPTIONAL)" type="text" name="friends[]"/><br /> <input type="hidden" name="friends_opt_in_path" value="OPE8B3F738CF07CE0C3AFA3F45A5E155ED" /><br>
+            </div>
+
+            <div style="clear: both;">
+            <input type="submit" value="SUBMIT" class="signup_button" id="center"/>
+            </div>  
+        </form>
+      </div>
+
+      <div style="clear: both;"></div>
+      <a name="contact"></a>
+      <div class="contact">
+          <h2>WE'RE FIGURING IT OUT WITH YOU</h2>
+          <p>
+              50% of women report that they experience feelings of self-doubt about their performance and careers - compared to just 31% of men. We want to change that.<br><br> 
+              As three close friends who met at work, we regularly leaned on each other for support, advice, and confidence tips and recognized a gap in the market. 
+              How could we scale the support we gave each other for women across the country? That's how Shine came to be. We are three women of color with separate backgrounds in product, marketing and engineering, 
+              curating content we need and want in our day to day.
+              <br><br>Have suggestions, thoughts, or just want to get in touch? We'd love to hear from you! <a href="mailto:theshinetext@gmail.com" target="_top">Contact us.</a>
+          </p>
+      </div>
+
+      <ul class="footer">
+          <h1>SHINE</h1>
+              <div id="footer-social">
+                  <li><a href="https://twitter.com/ShineText" target="_blank"><img src={{asset('images/twitter_yellow.png')}}></a></li>
+                  <li><a href="https://instagram.com/ShineText" target="_blank"><img src={{asset('images/instagram_yellow.png')}}></a></li>
+                  <li><a href="https://www.facebook.com/Shine-Text" target="_blank"><img src={{asset('images/facebook_yellow.png')}}></a></li>
+              </div>
+      </div>
+
+
 
 
     </body>
