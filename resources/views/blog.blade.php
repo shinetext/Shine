b1f035670d29aa661659d533f0ebe78559263354
Chloe Lee <chloealessandra@gmail.com>
2015-11-12 21:40:44 -0500

    adds blog coming soon in font and colors

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,1 +1,40 @@
-<p>Hi I'm the blog</p>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+    @font-face {
+        font-family: 'Bebas Neue Bold';
+        src: url('fonts/BebasNeueBold.eot');
+        src: url('fonts/BebasNeueBold.eot?#iefix') format('embedded-opentype'),
+            url('fonts/BebasNeueBold.woff') format('woff'),
+            url('fonts/BebasNeueBold.ttf') format('truetype'),
+            url('fonts/BebasNeueBold.svg#FontName') format('svg');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    @font-face {
+        font-family: 'Bebas Neue Regular';
+        src: url('fonts/BebasNeueRegular.eot');
+        src: url('fonts/BebasNeueRegular.eot?#iefix') format('embedded-opentype'),
+            url('fonts/BebasNeueRegular.woff') format('woff'),
+            url('fonts/BebasNeueRegular.ttf') format('truetype'),
+            url('fonts/BebasNeueRegular.svg#FontName') format('svg');
+        font-weight: normal;
+        font-style: normal;
+    }
+	.message {
+		font-family: "Bebas Neue Bold";
+		background-color: #E6E6E6;
+		font-size: 100px;
+		text-align: center;
+		padding: 10% 0 100% 0;
+	}
+</style>
+</head>
+<body>
+	<div class="message">
+		<p>Blog Coming Soon</p>
+	</div>
+</body>
+</html>
