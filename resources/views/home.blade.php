1f18990fdc4131cdb2b8e362274daa6b803820b6
Chloe Lee <chloealessandra@gmail.com>
2016-02-01 19:58:09 -0500

    adds meta tag to verify google business account

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,6 +23,7 @@
         <meta name="theme-color" content="#ffffff">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="nfSnOY6xfrE4rTipDFpxi97Wf0sF_nLoR9mh_Rtm1eo" />
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
