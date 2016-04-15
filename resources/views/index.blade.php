8b91d5e31f57126df8c447e2def9272dd62bed52
jon uy <jonxuy@gmail.com>
2016-04-15 16:17:19 -0400

    Moves referral and press section above the testimonials Also changes up how the anchors work a little when navigating around with the nav bar dropdown. Should feel like it's more accurately taking you to the section you're trying to go to.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,23 +29,22 @@
     <h3>Daily work and life advice sent to you in a text message every morning. We got you.</h3>
 
     <form action="https://secure.mcommons.com/profiles/join" method="post">
-        <input type="hidden" name="opt_in_path[]" value="OP4B1A27AC508266A1F4373419CE1BE391" />
-        <input type="hidden" name="redirect_to" value={{ URL::route('confirmation') }} />
+      <input type="hidden" name="opt_in_path[]" value="OP4B1A27AC508266A1F4373419CE1BE391" />
+      <input type="hidden" name="redirect_to" value={{ URL::route('confirmation') }} />
 
-        <input required="required" placeholder="First Name" type="text" name="person[first_name]" id="person_first_name" /></br>
+      <input required="required" placeholder="First Name" type="text" name="person[first_name]" id="person_first_name" /></br>
 
-        <input placeholder="Last Name (optional)" type="text" name="person[last_name]" id="person_last_name" /></br>
+      <input placeholder="Last Name (optional)" type="text" name="person[last_name]" id="person_last_name" /></br>
 
-        <input required="required" placeholder="Phone Number" type="text" name="person[phone]" id="person_phone" /></br>
+      <input required="required" placeholder="Phone Number" type="text" name="person[phone]" id="person_phone" /></br>
 
-        <input type="submit" value="SIGNUP" class="signup_button"/>
+      <input type="submit" value="SIGNUP" class="signup_button"/>
     </form>
 
-      <!-- <span style="font-size:80%"><a href="https://www.mobilecommons.com/about/terms-and-conditions/">Msg &amp; Data Rates May Apply.</a><br />Text <b>STOP</b> to opt out. No purchase necessary.<br />Expect 4 msgs/mo. <a href="https://www.mobilecommons.com/about/terms-and-conditions/privacy-policy/">Terms and Conditions</a></span> -->
   </div>
 </div>
 
-<a name="about"></a>
+<a name="about" class="anchor"></a>
 <div class="col-md-4">
   <img src={{asset('images/coffee_icon.png')}}>
   <h3>A.M. MOTIVATION</h3>
@@ -61,58 +60,41 @@
 <div class="col-md-4">
   <img src={{asset('images/mail_icon.png')}} id="mail">
   <h3>SHARE WITH FRIENDS</h3>
-  <a name="testimonials"></a>
   <p>The best part? You can share all of this motiv-affirmation (motivation + affirmation. Yeah, we made up a word) with your friends to give them the boost they need. After you join the movement of living with intentionality, invite your friends to be their best selves too!</p>
 </div>
 
 <div style="clear: both;"></div>
-<br><br><br><br><br><br><br>
-<div class="testimonials">
-  <!-- Used this tutorial to create carousel: https://bootstrapbay.com/blog/bootstrap-3-carousel-tutorial/ -->
-  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="5000">
-  <h2>WORD ON THE STREET</h2>
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-      <!-- <li data-target="#carousel-example-generic" data-slide-to="2"></li> -->
-    </ol>
+<div class="refer">
+  <a name="refer" class="anchor"></a>
+  <h2>Share Shine texts with your friends!</h2>
+  <form action="https://secure.mcommons.com/profiles/join" method="post">
+    <div class="refer-form">
+      <input type="hidden" name="opt_in_path[]" value="OP4B1A27AC508266A1F4373419CE1BE391" />
+      <input type="hidden" name="redirect_to" value={{ URL::route('refer-confirmation') }} />
 
-    <!-- Wrapper for slides - dimensions are screenshots of 1440 x 610 -->
-    <div class="carousel-inner">
-      <div class="item active">
-        <img src={{asset('images/testimonials_1.png')}}>
-<!--               <div class="carousel-caption">
-            <h3>Caption</h3>
-        </div> -->
+      <div class="col-md-4" id="referral">
+        <input required="required" placeholder="Your Name" type="text" name="person[first_name]" id="person_first_name" /></br></br>
+        <input required="required" placeholder="Your Phone Number" type="text" name="person[phone]" id="person_phone" /></br>
       </div>
-      <div class="item">
-        <img src={{asset('images/testimonials_2.png')}}>
+
+      <div class="col-md-4" id="referral">
+        <input required="required" placeholder="Your Friend's Number" type="text" name="friends[]"/><br /> <input type="hidden" name="friends_opt_in_path" value="OPE8B3F738CF07CE0C3AFA3F45A5E155ED" /><br>
+        <input placeholder="Your Friend's Number (optional)" type="text" name="friends[]"/><br /> <input type="hidden" name="friends_opt_in_path" value="OPE8B3F738CF07CE0C3AFA3F45A5E155ED" /><br>
+      </div>
+
+      <div class="col-md-4" id="referral">
+        <input placeholder="Your Friend's Number (optional)" type="text" name="friends[]"/><br /> <input type="hidden" name="friends_opt_in_path" value="OPE8B3F738CF07CE0C3AFA3F45A5E155ED" /><br>
+        <input placeholder="Your Friend's Number (optional)" type="text" name="friends[]"/><br /> <input type="hidden" name="friends_opt_in_path" value="OPE8B3F738CF07CE0C3AFA3F45A5E155ED" /><br>
       </div>
     </div>
 
-    <!-- Controls -->
-    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-    </a>
-    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-    </a>
-  </div> <!-- Carousel -->
-<!--    <div class="col-md-4" id="testimonials">
-    <div class="rays">
-      <img src={{asset('images/sun_rays.png')}}>
-    </div>
-    <div id="user_pic">
-      <img src={{asset('images/brenna.png')}}>
-    </div>
-    <p>"EVERYTHING I ever needed to make it through another awkward week of my version of adulting. I'm always twiddling my thumbs waiting for Shine to kick my ass back into gear. #dependency"</p>
-    <p id="name">Brenna, Graduate Student</p>
-  </div> -->
-<a name="press"></a>
+    <input type="submit" value="SUBMIT" class="signup_button" id="center"/>
+    <br><br><br>
+  </form>
 </div>
 
 <div class="press">
+  <a name="press" class="anchor"></a>
   <h2>In the news</h2>
     <li id="cnn"><a href="http://money.cnn.com/2016/01/29/technology/startup-shine-text/index.html" target="_blank"><img src={{asset('images/CNNMoney_logo.png')}}></a></li>
     <div class="hidden-sm hidden-md hidden-lg"><br></div>
@@ -126,39 +108,41 @@
   </div>
 </div>
 
-<a name="refer"></a>
-<div style="clear: both;">
-<div class="refer-a-friend">
-  <h2>Share Shine texts with your friends!</h2>
-  <form action="https://secure.mcommons.com/profiles/join" method="post">
-      <div class="refer_form">
-        <input type="hidden" name="opt_in_path[]" value="OP4B1A27AC508266A1F4373419CE1BE391" />
-            <input type="hidden" name="redirect_to" value={{ URL::route('refer-confirmation') }} />
+<div style="clear: both;"></div>
+<br><br><br><br><br><br><br>
+<div class="testimonials">
+  <a name="testimonials" class="anchor"></a>
+  <!-- Used this tutorial to create carousel: https://bootstrapbay.com/blog/bootstrap-3-carousel-tutorial/ -->
+  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="5000">
+    <h2>WORD ON THE STREET</h2>
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    </ol>
 
-        <div class="col-md-4" id="referral">
-            <input required="required" placeholder="Your Name" type="text" name="person[first_name]" id="person_first_name" /></br></br>
-            <input required="required" placeholder="Your Phone Number" type="text" name="person[phone]" id="person_phone" /></br>
-        </div>
-
-        <div class="col-md-4" id="referral">
-            <input required="required" placeholder="Your Friend's Number" type="text" name="friends[]"/><br /> <input type="hidden" name="friends_opt_in_path" value="OPE8B3F738CF07CE0C3AFA3F45A5E155ED" /><br>
-            <input placeholder="Your Friend's Number (optional)" type="text" name="friends[]"/><br /> <input type="hidden" name="friends_opt_in_path" value="OPE8B3F738CF07CE0C3AFA3F45A5E155ED" /><br>
-        </div>
-
-        <div class="col-md-4" id="referral">
-            <input placeholder="Your Friend's Number (optional)" type="text" name="friends[]"/><br /> <input type="hidden" name="friends_opt_in_path" value="OPE8B3F738CF07CE0C3AFA3F45A5E155ED" /><br>
-            <input placeholder="Your Friend's Number (optional)" type="text" name="friends[]"/><br /> <input type="hidden" name="friends_opt_in_path" value="OPE8B3F738CF07CE0C3AFA3F45A5E155ED" /><br>
-        </div>
+    <!-- Wrapper for slides - dimensions are screenshots of 1440 x 610 -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src={{asset('images/testimonials_1.png')}}>
       </div>
-
-      <input type="submit" value="SUBMIT" class="signup_button" id="center"/>
-      <br><br><br>
+      <div class="item">
+        <img src={{asset('images/testimonials_2.png')}}>
       </div>
-  </form>
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+    </a>
+  </div>
 </div>
 
-<a name="partners"></a>
 <div class="partners">
+  <a name="partners" class="anchor"></a>
   <h4>Partners We Love</h4>
   <li id="dreamers"><a href="http://www.dreamersdoers.me/" target="_blank"><img src={{asset('images/dreamers_and_doers.png')}}></a></li>
   <li id="first"><a href="https://www.shesthefirst.org/" target="_blank"><img src={{asset('images/shes_the_first.png')}}></a></li>
